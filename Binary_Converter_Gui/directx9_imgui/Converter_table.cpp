df99cc5f44270617b2a169141762d34e88eda618
Arthur <71910656+ArthurF23@users.noreply.github.com>
2021-05-19 18:31:07 -0400

    Revamp to some code

--- Binary_Converter_Gui/directx9_imgui/Converter_table.cpp
+++ Binary_Converter_Gui/directx9_imgui/Converter_table.cpp
@@ -1,303 +1,94 @@
 #include <string>
+#include <iostream>
 using namespace std;
 #include "Converter_table.h"
 
+//Divides inputed number by 2 and checks to see if it is an integer or not
+inline bool check_int_simplify(int inp) {
+	double placeholder = inp;
+
+	placeholder = placeholder / 2;
+	//If the double is an integer it will equal the converted to int double
+	if (placeholder == (int)placeholder) {
+		return false;
+	}
+	else {
+		return true;
+	};
+}
+
+//Adds zeros to make sure all binary strings are 8-bit
+inline string eight_bitify(string inp) {
+	int add_bits = 8 - inp.length();
+	if (add_bits > 0) {
+		for (int i = 0; i < add_bits; i++)
+			inp = "0" + inp;
+	};
+	return inp + " ";
+}
+
+//Converts decimal to binary by dividing by 2 and using check_int_simplify
+inline string decimal_to_bin(int dec) {
+	string bin = "";
+	while (dec != 0) {
+		//If true, binary bit is 1
+		if (check_int_simplify(dec) == true) {			
+			bin = bin + "1";
+		}
+		//If false, binary bit is 0
+		else {			
+			bin = bin + "0";
+		}
+		//Divide input by 2 as that is how the converson operates
+		dec = dec / 2;		
+	}
+	return (eight_bitify(bin));
+}
+//Converts binary to ASCII char
+inline char bin_to_ascii(string bin) {
+	//gets all 8 bits of binary string
+	char positions[8] = { bin[0], bin[1], bin[2], bin[3], bin[4], bin[5], bin[6], bin[7] };
+	int result = 2;
+	for (int i = 0; i < 8; i++) {
+		//If bit is one it converts said bit to decimal number
+		if (positions[i] == '1') {
+			if (8 - (i + 1) != 0) {
+				//Multiplies it x times based on it's bit position
+				for (int u = 1; u < (8 - (i + 1)); u++) {
+					result = result * 2;
+				};
+			}
+			//If the exponent would be 0 then it adds one
+			else {
+				result++;
+			};
+		};
+	};
+	//returns ascii decimal value
+	return result;
+};
+
+//Gets decimal code for the ASCII character
+template <typename ascii_to_decimal_>
+inline int ascii_to_decimal(char inp) { return int(inp); };
+
+
 namespace std {
 
+	//Converts ASCII text to Binary by first converting it to decimal and then converting to binary
 	string Convert::ASCII_TO_BIN(char input) {
-		static string bin_str;
-		switch (input) {
-
-		case 'A':
-			bin_str = "01000001";
-			break;
-
-		case 'B':
-			bin_str = "01000010";
-			break;
-
-		case 'C':
-			bin_str = "01000011";
-			break;
-
-		case 'D':
-			bin_str = "01000100";
-			break;
-
-		case 'E':
-			bin_str = "01000101";
-			break;
-
-		case 'F':
-			bin_str = "01000110";
-			break;
-
-		case 'G':
-			bin_str = "01000111";
-			break;
-
-		case 'H':
-			bin_str = "01001000";
-			break;
-
-		case 'I':
-			bin_str = "01001001";
-			break;
-
-		case 'J':
-			bin_str = "01001010";
-			break;
-
-		case 'K':
-			bin_str = "01001011";
-			break;
-
-		case 'L':
-			bin_str = "01001100";
-			break;
-
-		case 'M':
-			bin_str = "01001101";
-			break;
-
-		case 'N':
-			bin_str = "01001110";
-			break;
-
-		case 'O':
-			bin_str = "01001111";
-			break;
-
-		case 'P':
-			bin_str = "01010000";
-			break;
-
-		case 'Q':
-			bin_str = "01010001";
-			break;
-
-		case 'R':
-			bin_str = "01010010";
-			break;
-
-		case 'S':
-			bin_str = "01010011";
-			break;
-
-		case 'T':
-			bin_str = "01010100";
-			break;
-
-		case 'U':
-			bin_str = "01010101";
-			break;
-
-		case 'V':
-			bin_str = "01010110";
-			break;
-
-		case 'W':
-			bin_str = "01010111";
-			break;
-
-		case 'X':
-			bin_str = "01011000";
-			break;
-
-		case 'Y':
-			bin_str = "01011001";
-			break;
-
-		case 'Z':
-			bin_str = "01011010";
-			break;
-
-		case 'a':
-			bin_str = "01100001";
-			break;
-
-		case 'b':
-			bin_str = "01100010";
-			break;
-
-		case 'c':
-			bin_str = "01100011";
-			break;
-
-		case 'd':
-			bin_str = "01100100";
-			break;
-
-		case 'e':
-			bin_str = "01100101";
-			break;
-
-		case 'f':
-			bin_str = "01100110";
-			break;
-
-		case 'g':
-			bin_str = "01100111";
-			break;
-
-		case 'h':
-			bin_str = "01101000";
-			break;
-
-		case 'i':
-			bin_str = "01101001";
-			break;
-
-		case 'j':
-			bin_str = "01101010";
-			break;
-
-		case 'k':
-			bin_str = "01101011";
-			break;
-
-		case 'l':
-			bin_str = "01101100";
-			break;
-
-		case 'm':
-			bin_str = "01101101";
-			break;
-
-		case 'n':
-			bin_str = "01101110";
-			break;
-
-		case 'o':
-			bin_str = "01101111";
-			break;
-
-		case 'p':
-			bin_str = "01110000";
-			break;
-
-		case 'q':
-			bin_str = "01110001";
-			break;
-
-		case 'r':
-			bin_str = "01110010";
-			break;
-
-		case 's':
-			bin_str = "01110011";
-			break;
-
-		case 't':
-			bin_str = "01110100";
-			break;
-
-		case 'u':
-			bin_str = "01110101";
-			break;
-
-		case 'v':
-			bin_str = "01110110";
-			break;
-
-		case 'w':
-			bin_str = "01110111";
-			break;
-
-		case 'x':
-			bin_str = "01111000";
-			break;
-
-		case 'y':
-			bin_str = "01111001";
-			break;
-
-		case 'z':
-			bin_str = "01111010";
-			break;
-
-		case ' ':
-			bin_str = "00100000";
-			break;
-
-		case '!':
-			bin_str = "00100001";
-			break;
-
-		case '?':
-			bin_str = "00111111";
-			break;
-
-		case ',':
-			bin_str = "00101100";
-			break;
-
-		case '\'':
-			bin_str = "00100111";
-			break;
-
-		case '"':
-			bin_str = "00100010";
-			break;
-
-		case '.':
-			bin_str = "00101110";
-			break;
-
-		case ';':
-			bin_str = "00111011";
-			break;
-
-		case '0':
-			bin_str = "00110000";
-			break;
-
-		case '1':
-			bin_str = "00110001";
-			break;
-
-		case '2':
-			bin_str = "00110010";
-			break;
-
-		case '3':
-			bin_str = "00110011";
-			break;
-
-		case '4':
-			bin_str = "00110100";
-			break;
-
-		case '5':
-			bin_str = "00110101";
-			break;
-
-		case '6':
-			bin_str = "00110110";
-			break;
-
-		case '7':
-			bin_str = "00110111";
-			break;
-
-		case '8':
-			bin_str = "00111000";
-			break;
-
-		case '9':
-			bin_str = "00111001";
-			break;
-
-		default:
-			bin_str = "";
-			break;
+		if (input == (char)"" || input == NULL) {
+			return "";
 		}
-		return bin_str;
-	}
+		else {
+			return decimal_to_bin(ascii_to_decimal<char>(input)) + " ";
+		};
+	};
 	
 
 	char Convert::BINARY_TO_HEX(string inp) {
 		static char hex_bin;
+		//All hex values are easily represented by 4/8 bits of binary so its easy to make a simple table to convert all values
 		if (inp == "0000") { hex_bin = '0'; }
 		else if (inp == "0001") { hex_bin = '1'; }
 		else if (inp == "0010") { hex_bin = '2'; }
@@ -315,12 +106,13 @@ namespace std {
 		else if (inp == "1101") { hex_bin = 'D'; }
 		else if (inp == "1110") { hex_bin = 'E'; }
 		else if (inp == "1111") { hex_bin = 'F'; }
-		return hex_bin;
+		return hex_bin;		
 	}
 
 
 	string Convert::HEX_TO_BINARY(char inp) {
 		static string bin_val;
+		// All hex values are easily represented by 4 / 8 bits of binary so its easy to make a simple table to convert all values as in the Convert::BINARY_TO_HEX function
 		switch (inp) {
 
 		case '0':
@@ -398,84 +190,6 @@ namespace std {
 	}
 
 	char Convert::BIN_TO_ASCII(string inp) {
-		static char placeholder;
-
-		if (inp == "01000001") { placeholder = 'A'; }
-		else if (inp == "01000010") { placeholder = 'B'; }
-		else if (inp == "01000011") { placeholder = 'C'; }
-		else if (inp == "01000100") { placeholder = 'D'; }
-		else if (inp == "01000101") { placeholder = 'E'; }
-		else if (inp == "01000110") { placeholder = 'F'; }
-		else if (inp == "01000111") { placeholder = 'G'; }
-		else if (inp == "01001000") { placeholder = 'H'; }
-		else if (inp == "01001001") { placeholder = 'I'; }
-		else if (inp == "01001010") { placeholder = 'J'; }
-		else if (inp == "01001011") { placeholder = 'K'; }
-		else if (inp == "01001100") { placeholder = 'L'; }
-		else if (inp == "01001101") { placeholder = 'M'; }
-		else if (inp == "01001110") { placeholder = 'N'; }
-		else if (inp == "01001111") { placeholder = 'O'; }
-		else if (inp == "01010000") { placeholder = 'P'; }
-		else if (inp == "01010001") { placeholder = 'Q'; }
-		else if (inp == "01010010") { placeholder = 'R'; }
-		else if (inp == "01010011") { placeholder = 'S'; }
-		else if (inp == "01010100") { placeholder = 'T'; }
-		else if (inp == "01010101") { placeholder = 'U'; }
-		else if (inp == "01010110") { placeholder = 'V'; }
-		else if (inp == "01010111") { placeholder = 'W'; }
-		else if (inp == "01011000") { placeholder = 'X'; }
-		else if (inp == "01011001") { placeholder = 'Y'; }
-		else if (inp == "01011010") { placeholder = 'Z'; }
-
-		else if (inp == "01100001") { placeholder = 'a'; }
-		else if (inp == "01100010") { placeholder = 'b'; }
-		else if (inp == "01100011") { placeholder = 'c'; }
-		else if (inp == "01100100") { placeholder = 'd'; }
-		else if (inp == "01100101") { placeholder = 'e'; }
-		else if (inp == "01100110") { placeholder = 'f'; }
-		else if (inp == "01100111") { placeholder = 'g'; }
-		else if (inp == "01101000") { placeholder = 'h'; }
-		else if (inp == "01101001") { placeholder = 'i'; }
-		else if (inp == "01101010") { placeholder = 'j'; }
-		else if (inp == "01101011") { placeholder = 'k'; }
-		else if (inp == "01101100") { placeholder = 'l'; }
-		else if (inp == "01101101") { placeholder = 'm'; }
-		else if (inp == "01101110") { placeholder = 'n'; }
-		else if (inp == "01101111") { placeholder = 'o'; }
-		else if (inp == "01110000") { placeholder = 'p'; }
-		else if (inp == "01110001") { placeholder = 'q'; }
-		else if (inp == "01110010") { placeholder = 'r'; }
-		else if (inp == "01110011") { placeholder = 's'; }
-		else if (inp == "01110100") { placeholder = 't'; }
-		else if (inp == "01110101") { placeholder = 'u'; }
-		else if (inp == "01110110") { placeholder = 'v'; }
-		else if (inp == "01110111") { placeholder = 'w'; }
-		else if (inp == "01111000") { placeholder = 'x'; }
-		else if (inp == "01111001") { placeholder = 'y'; }
-		else if (inp == "01111010") { placeholder = 'z'; }
-
-		else if (inp == "00100000") { placeholder = ' '; }
-
-		else if (inp == "00100001") { placeholder = '!'; }
-		else if (inp == "00111111") { placeholder = '?'; }
-		else if (inp == "00101100") { placeholder = ','; }
-		else if (inp == "00100010") { placeholder = '"'; }
-		else if (inp == "00100111") { placeholder = '\''; }
-		else if (inp == "00101110") { placeholder = '.'; }
-		else if (inp == "00111011") { placeholder = ';'; }
-
-
-		else if (inp == "00110000") { placeholder = '0'; }
-		else if (inp == "00110001") { placeholder = '1'; }
-		else if (inp == "00110010") { placeholder = '2'; }
-		else if (inp == "00110011") { placeholder = '3'; }
-		else if (inp == "00110100") { placeholder = '4'; }
-		else if (inp == "00110101") { placeholder = '5'; }
-		else if (inp == "00110110") { placeholder = '6'; }
-		else if (inp == "00110111") { placeholder = '7'; }
-		else if (inp == "00111000") { placeholder = '8'; }
-		else if (inp == "00111001") { placeholder = '9'; }
-
-		return placeholder;
+		return bin_to_ascii(inp);
 	}
 }
--- Binary_Converter_Gui/directx9_imgui/Converter_table.h
+++ Binary_Converter_Gui/directx9_imgui/Converter_table.h
@@ -6,12 +6,13 @@ namespace std {
 	class Convert {
 
 	public:
+		//Converts ASCII text to Binary
 		static string ASCII_TO_BIN(char input);
-
+		//Converts Binary to ASCII text
 		static char BIN_TO_ASCII(string inp);		
-
+		//Converts Hexadecimal to Binary
 		static string HEX_TO_BINARY(char inp);
-
+		//Converts Binary to Hexadecimal
 		static char BINARY_TO_HEX(string inp);
 	};
 }
